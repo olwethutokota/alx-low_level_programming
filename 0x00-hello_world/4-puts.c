509a4a3892cf201910bcaf366765659e26115b8b
olwethutokota <olwethutokota13@gmail.com>
2023-08-10 03:26:09 -0700

    4 puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,12 @@
-/*
- * File: 4-puts.c
- * Auth: Olwethu Tokota  <olwethutokota13@gmail.com>
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-i}
+}
