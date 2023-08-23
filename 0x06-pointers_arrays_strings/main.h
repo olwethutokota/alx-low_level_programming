12bc50ca35b377f69154b3fb04bf205812613d3f
olwethutokota <olwethutokota13@gmail.com>
2023-08-23 09:57:01 -0700

    print

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,6 +17,8 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
+int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 
