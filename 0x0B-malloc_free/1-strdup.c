4aa557e2e8a77aefaeaa6da3981977ca506ae880
My Name <olwethutokota13@gmail.com>
2023-09-04 14:52:14 -0700

    worrk

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,46 +1,37 @@
-#include <stdlib.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _strdup - copy of the string given as a parameter.
- *
- * @str: unsigned int
- *
- * Return: char pointer
- */
+* _strdup - returns a pointer to a newly allocated
+*space in memory, which contains a copy of the
+*string given as a parameter.
+*@str:String to be copied
+*
+*Return: NULL in case of error, pointer to allocated
+*space
+*/
 
 char *_strdup(char *str)
 {
-	unsigned int counter1, counter2;
-	char *strCopy;
-
-	counter1 = 0;
-	counter2 = 0;
+	char *cpy;
+	int index, len;
 
 	if (str == NULL)
-	{
-		return (0);
-	}
-
-	while (str[counter1] != '\0')
-	{
-		counter1++;
-	}
-
-	strCopy = malloc(sizeof(char) * (counter1 + 1));
-
-	if (strCopy == NULL)
-	{
 		return (NULL);
-	}
 
-	while (counter2 < counter1)
+	for (index = 0; str[index]; index++)
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
+
+	if (cpy == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
 	{
-		strCopy[counter2] = str[counter2];
-		counter2++;
+		cpy[index] = str[index];
 	}
 
-	strCopy[counter2] = '\0';
+	cpy[len] = '\0';
 
-	return (strCopy);
+	return (cpy);
+
 }
